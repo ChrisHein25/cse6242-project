fc7338ac9e64e4b012e4940676e1cdbbefc11c23
ChrisHein25 <chris.hein@comcast.net>
2021-10-29 11:55:54 -0400

    added new python file

---
+++ game_data.py
@@ -0,0 +1,48 @@
+# CSE 6242 Group 9 Project
+# Python script to access nba.com stat data and pull stats on a per-game basis.
+# Output is a table consisting of individual player stats for each NBA game played by each team for the past X seasons
+
+from nba_api.stats.static import players
+from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import playergamelog, playergamelogs
+
+from datetime import datetime
+import pandas as pd
+from pandasql import sqldf
+
+# setup helper functions
+mysql = lambda q: sqldf(q, globals()) # setup to run sql queries on data
+
+# enter starting and ending seasons of interest
+season_start_startyear = 2003
+seasons_end_startyear = 2018 # starting season year, eg 2019 would be 2019-2020 season
+seasons = list(range(season_start_startyear, seasons_end_startyear+1))
+
+# grab all NBA players
+player_dict = players.get_players() # dictionary of every player who has played in the NBA
+
+# for every season of interest, call PlayerGameLogs to get game stats from that season
+frames = []
+for i, season in enumerate(seasons):
+    season_str = str(season)+"-"+str(season+1)[2:]
+    df = playergamelogs.PlayerGameLogs(season_nullable=season_str).get_data_frames()[0]
+    frames.append(df)
+game_data = pd.concat(frames)
+game_data = game_data.drop(columns=[
+    "GP_RANK","W_RANK","L_RANK","W_PCT_RANK","MIN_RANK","FGM_RANK","FGA_RANK","FG_PCT_RANK","FG3M_RANK","FG3A_RANK",
+    "FG3_PCT_RANK","FTM_RANK","FTA_RANK","FT_PCT_RANK","OREB_RANK","DREB_RANK","REB_RANK","AST_RANK","TOV_RANK",
+    "STL_RANK","BLK_RANK","BLKA_RANK","PF_RANK","PFD_RANK","PTS_RANK","PLUS_MINUS_RANK","NBA_FANTASY_PTS_RANK",
+    "DD2_RANK","TD3_RANK","VIDEO_AVAILABLE_FLAG"
+]) # remove unneeded ranking columns
+game_data = game_data.drop_duplicates() # drop any duplicate rows
+game_data['GAME_DATE']= pd.to_datetime(game_data['GAME_DATE']) # clean up game date column to pd date
+game_data['GAME_DATE'] = game_data['GAME_DATE'].dt.date # eliminate time part of datetime column (all 0's anyway)
+
+# the resulting dataframe game_data contains core player stats on a per game basis for every player for every game in the season range selected
+game_data.to_csv("game_data.csv", index=False) # write to CSV
+
+# query the dataframe with SQL
+res = mysql(""" select * from game_data where season_year="2015-16"; """)
+
+# TODO - pull more unique/advanced stats next
--- nba_api_test.py
+++ nba_api_test.py
@@ -13,3 +13,43 @@ GSW = [x for x in teams if x['full_name'] == 'Golden State Warriors'][0]
 GSW_id = GSW['id']
 
 print(bron_id)
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# # for each NBA player, grab season data for seasons of interest
+# for cnt, player in enumerate(player_dict):
+#     print(cnt)
+#     if cnt == 0:
+#         player_gamelog = playergamelog.PlayerGameLog(player_id=player['id'], season=SeasonAll.all).get_data_frames()[0]
+#     else:
+#         player_gamelog_to_add = playergamelog.PlayerGameLog(player_id=player['id'], season=SeasonAll.all).get_data_frames()[0]
+#         frames = [player_gamelog, player_gamelog_to_add]
+#         player_gamelog = pd.concat(frames)
+#
+# print(cnt)
+# #
+# playergamelog.PlayerGameLog(player_id='2544', season = SeasonAll.all)
+#
+# test = playergamelog.PlayerGameLog(player_id='2544', season='all')
+#
+# #Converts gamelog object into a pandas dataframe
+# #can also convert to JSON or dictionary
+# df_bron_games_all = gamelog_bron.get_data_frames()
+#
+# # If you want all seasons, you must import the SeasonAll parameter
+# from nba_api.stats.library.parameters import SeasonAll
+#
+# gamelog_bron_all = playergamelog.PlayerGameLog(player_id='2544', season = SeasonAll.all)
+#
+# df_bron_games_all = gamelog_bron_all.get_data_frames()
+#
